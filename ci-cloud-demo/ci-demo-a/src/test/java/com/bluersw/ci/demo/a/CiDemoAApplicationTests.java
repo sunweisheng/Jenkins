19fbc65cc18ad84f3007254a1c80914b62f1541f
sunweisheng <sunweisheng@live.cn>
2019-11-24 21:30:55 +0800

    添加微服务构建示例项目

---
+++ ci-cloud-demo/ci-demo-a/src/test/java/com/bluersw/ci/demo/a/CiDemoAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluersw.ci.demo.a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiDemoAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
