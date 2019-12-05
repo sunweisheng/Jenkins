7b0d873d2eb57b29763b6ab2b0ebfbc27c8d890c
SunWeiSheng <sunweisheng@live.cn>
2019-12-05 18:45:32 +0800

    完成复选框选择

--- jenkins-plugin-microserviceparameter/src/main/java/com/bluersw/jenkins/plugin/microserviceparameter/MicroServiceParameterDefinition.java
+++ jenkins-plugin-microserviceparameter/src/main/java/com/bluersw/jenkins/plugin/microserviceparameter/MicroServiceParameterDefinition.java
@@ -1,7 +1,8 @@
 package com.bluersw.jenkins.plugin.microserviceparameter;
 
 import java.io.IOException;
-
+import java.util.UUID;
+import java.util.logging.Logger;
 import javax.annotation.CheckForNull;
 import javax.servlet.ServletException;
 
@@ -18,20 +19,37 @@ import org.kohsuke.stapler.StaplerRequest;
 //微服务构建参数类，构建参数必须继承ParameterDefinition，继承后可以在Add Parameter选项中出现
 public class MicroServiceParameterDefinition extends ParameterDefinition {
 
-	private String subdirectory;
+	private static final Logger LOG = Logger.getLogger(MicroServiceParameterDefinition.class.getName());
+
+	//serialVersionUID串行化后的版本号用于在并行化时进行比较
+	static final long serialVersionUID = 5;
+	//构建时分组循环构建的分组名称组合，用","分割，多用于微服务项目构建。
+	private String profiles;
+
+	private final UUID uuid;
 
 	//构造函数
 	//@DataBoundConstructor注解告诉Jenkins用config.jelly中的Fields进行实例化
 	@DataBoundConstructor
-	public MicroServiceParameterDefinition(String name, String description, String subdirectory) {
+	public MicroServiceParameterDefinition(String name, String description, String profiles) {
 		super(name, description);
-		this.subdirectory = subdirectory;
+		this.profiles = profiles;
+		this.uuid = UUID.randomUUID();
 	}
 
-	public String getSubdirectory(){return this.subdirectory;}
+	//创建DIV元素的ID
+	public String getDivUUID() {
+		StringBuilder randomDIVID = new StringBuilder();
+		randomDIVID.append(getName()).append("-").append(uuid);
+		return randomDIVID.toString();
+	}
 
-	public void setSubdirectory(String subdirectory){
-		this.subdirectory = subdirectory;
+
+
+	public String getProfiles(){return this.profiles;}
+
+	public void setProfiles(String profiles){
+		this.profiles = profiles;
 	}
 
 	@CheckForNull
--- jenkins-plugin-microserviceparameter/src/main/resources/com/bluersw/jenkins/plugin/microserviceparameter/javascript/ms-parameter.js
+++ jenkins-plugin-microserviceparameter/src/main/resources/com/bluersw/jenkins/plugin/microserviceparameter/javascript/ms-parameter.js
@@ -1,20 +1,49 @@
-function initSubdirectory(subdirectory,parentDiv){
-		var subdirectoryArray = subdirectory.split(",");
-		console.log(subdirectory)
+function initProfiles(profileDIV,profiles){
+		var profileArray = profiles.split(",");
+		console.log(profileArray)
 
-		for(var i=0;i<subdirectoryArray.length;i++){
-        add(parentDiv,subdirectoryArray[i],subdirectoryArray[i])
+		if(profileArray.length>1){
+			var checkBox = document.createElement("input");
+            checkBox.setAttribute("type", "checkbox");
+            checkBox.setAttribute("name", "com-bluersw-selectAll");
+            checkBox.setAttribute("value", "all");
+            checkBox.setAttribute("onclick", "selAll(this)");
+            var label = document.createElement("label");
+            label.innerHTML = "ALL";
+            profileDIV.appendChild(checkBox);
+            profileDIV.appendChild(label);
+		}
+
+		for(var i=0;i<profileArray.length;i++){
+        	addProfile(profileDIV,profileArray[i],profileArray[i])
 		}
 }
 
-function add(parentDiv,value,labelVel) {
+	function addProfile(profileDIV,value,labelVel) {
 		var checkBox = document.createElement("input");
 		checkBox.setAttribute("type", "checkbox");
-		checkBox.setAttribute("name", "selectSD");
+		checkBox.setAttribute("name", "selectProfile");
 		checkBox.setAttribute("value", value);
+		checkBox.setAttribute("onclick", "sel(this)");
 		var label = document.createElement("label");
 		label.innerHTML = labelVel;
-		console.log(parentDiv)
-		parentDiv.appendChild(checkBox);
-		parentDiv.appendChild(label);
+		console.log(profileDIV)
+		profileDIV.appendChild(checkBox);
+		profileDIV.appendChild(label);
+	}
+
+	//全选
+	function selAll(es) {
+		var selects = document.getElementsByName("selectProfile");
+		for (var i = 0; i < selects.length; i++) {
+			selects[i].checked = es.checked;
+		}
+	}
+
+	function sel(es) {
+		var elements = document.getElementsByName("com-bluersw-selectAll");
+		console.log(elements[0])
+		if(elements[0].checked && !es.checked){
+		elements[0].checked = false;
+		}
 	}
