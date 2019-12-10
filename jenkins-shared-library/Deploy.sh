c69960a128ff5cf1c96e698105cd5cc8e29acb24
SunWeiSheng <sunweisheng@live.cn>
2019-12-10 17:52:22 +0800

    add Deploy.sh

--- jenkins-shared-library/Deploy.sh
+++ jenkins-shared-library/Deploy.sh
@@ -5,3 +5,9 @@ rm -rf ../../jenkins-shared-library/vars
 cp -r ./shared-library/resources ../../jenkins-shared-library/resources
 cp -r ./shared-library/src ../../jenkins-shared-library/src
 cp -r ./shared-library/vars ../../jenkins-shared-library/vars
+
+cd ../../jenkins-shared-library/
+
+git add .
+git commit -m "部署"
+git push origin
