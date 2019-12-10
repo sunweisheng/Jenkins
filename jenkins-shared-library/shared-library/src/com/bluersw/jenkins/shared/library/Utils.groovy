c012ab66c4411df928a80b3d2681ef1af74888a7
sunweisheng <sunweisheng@live.cn>
2019-12-11 00:43:37 +0800

    test

--- jenkins-shared-library/shared-library/src/com/bluersw/jenkins/shared/library/Utils.groovy
+++ jenkins-shared-library/shared-library/src/com/bluersw/jenkins/shared/library/Utils.groovy
@@ -1,5 +1,9 @@
 package com.bluersw.jenkins.shared.library
 
+import hudson.model.Job
+import hudson.model.ParameterDefinition
+import hudson.model.ParametersDefinitionProperty
+
 class Utils implements Serializable{
 
 	def script
@@ -10,5 +14,12 @@ class Utils implements Serializable{
 
 	void TestPrint(){
 		this.script.println("Test Print OK")
+	}
+
+	void TestParam(){
+		Job job = this.script.$build().getParent()
+		ParametersDefinitionProperty paramsJobProperty = job.getProperty(ParametersDefinitionProperty.class)
+		ParameterDefinition ms = paramsJobProperty.getParameterDefinition("MicroServiceParameter")
+		this.script.println(ms.name)
 	}
 }
